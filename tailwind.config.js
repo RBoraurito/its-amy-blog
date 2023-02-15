3ecf1ad64af78e6bc69841d1dbb4e14b98ece295
Ricardo Boraure <r.boraure@kation.com.do>
2023-02-14 20:56:11 -0400

    🔧 Add primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
         title: ['var(--font-title)', ...fontFamily.sans],
         sans: ['var(--font-body)', ...fontFamily.sans],
       },
+      colors: {
+        primary: {
+          50: '#f1ed22',
+          100: '#f3f339',
+          200: '#f6f051',
+          300: '#f6eb51',
+          400: '#f1dc1e',
+          500: '#e8c811',
+          600: '#e8c811',
+          700: '#b8820f',
+          800: '#a57118',
+          900: '#a57118',
+        }
+      }
     },
   },
   plugins: [],
